a82e3c1c70f5de21744412f6efff505a922b7654
Antonio Nicolas Plata <Antonio.Plata@ibm.com>
2020-04-06 21:14:31 -0500

    next line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		System.out.println("Jenkins is working cloning the data from the repository automatically");
+		System.out.println("Im testing again this project");
 	}
 }
