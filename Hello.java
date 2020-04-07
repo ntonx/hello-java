8206a82ab14fc14c473ef552779f2faea0777d50
Antonio Nicolas Plata <Antonio.Plata@ibm.com>
2020-04-06 19:49:41 -0500

    updating for jenkins connection

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world");
+		System.out.println("Jenkins is working cloning the data from the repository automatically");
 	}
 }
