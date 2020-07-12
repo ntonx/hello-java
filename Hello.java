725773ba7b5f79157abedf897ab3d40e04e36f0d
Antonio Nicolas P <ntonx9@gmail.com>
2020-07-12 15:52:17 -0500

    testing user data 2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		System.out.println("Testing git hub practise");
+		System.out.println("Testing git hub practise 2");
 		System.out.println("Im testing again this project");
 	}
 }
