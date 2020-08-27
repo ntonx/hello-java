3ce9e7ee2bdc18e582b5b27a5d97b862392b5c32
Antonio Plata <ntonx9@gmail.com>
2020-08-26 23:00:07 -0500

    Change main from dev1 master

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		System.out.println("Editing inside branch");
-		System.out.println("Im testing again this project");
 	}
 }
