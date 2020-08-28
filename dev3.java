64a91291c340a7a0a29bc8694129f965c8ebe777
Antonio Plata <ntonx9@gmail.com>
2020-08-27 22:00:07 -0500

    create dev3

---
+++ dev3.java
@@ -0,0 +1,15 @@
+
+public class dev3{
+	public String myfunction(String data){	
+        return data;
+    }
+    
+    public String getName(String name){
+	    return name;
+    }
+    
+    public String getX(String name){
+	    return name;
+    }
+    
+}
